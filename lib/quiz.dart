0919ddff57ee115b4aa4d13c351629470cd26793
never47 <guseinovi2002@gmail.com>
2024-03-05 12:05:16 +0100

    add: questions_screen & quiz(for render content/lift up)

---
+++ lib/quiz.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/questions_scren.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget activeScreen = const StartScreen();
+
+  void changeScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 84, 30, 177),
+                Color.fromARGB(255, 107, 15, 170)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
