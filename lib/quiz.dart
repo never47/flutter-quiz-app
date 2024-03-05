1085823841702c59120edb877dd4b930477e2fea
never47 <guseinovi2002@gmail.com>
2024-03-05 13:04:03 +0100

    add: switch screen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -12,9 +12,15 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget activeScreen = const StartScreen();
+  Widget? activeScreen;
 
-  void changeScreen() {
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
     setState(() {
       activeScreen = const QuestionsScreen();
     });
